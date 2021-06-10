a1bcceb0550971e8242f73c15c8da18fb470f0a1
JungJun <ssikhj1214@gmail.com>
2021-06-10 18:57:38 +0900

    DB관련 패키지 추가

--- src/itTalkDO/Rb.java
+++ src/itTalkDO/Rb.java
@@ -2,4 +2,50 @@ package itTalkDO;
 
 public class Rb {
 
+	private int rb_no;
+	private int b_no;
+	private int mb_no;
+	private int rctg_no;
+	private String rb_write;
+	private String rb_date;
+	
+	public int getRb_no() {
+		return rb_no;
+	}
+	public void setRb_no(int rb_no) {
+		this.rb_no = rb_no;
+	}
+	public int getB_no() {
+		return b_no;
+	}
+	public void setB_no(int b_no) {
+		this.b_no = b_no;
+	}
+	public int getMb_no() {
+		return mb_no;
+	}
+	public void setMb_no(int mb_no) {
+		this.mb_no = mb_no;
+	}
+	public int getRctg_no() {
+		return rctg_no;
+	}
+	public void setRctg_no(int rctg_no) {
+		this.rctg_no = rctg_no;
+	}
+	public String getRb_write() {
+		return rb_write;
+	}
+	public void setRb_write(String rb_write) {
+		this.rb_write = rb_write;
+	}
+	public String getRb_date() {
+		return rb_date;
+	}
+	public void setRb_date(String rb_date) {
+		this.rb_date = rb_date;
+	} 
+	
+	
+	
 }
--- src/itTalkDO/Rctg.java
+++ src/itTalkDO/Rctg.java
@@ -2,4 +2,21 @@ package itTalkDO;
 
 public class Rctg {
 
+	private int rctg_no; 
+	private String rctg_name;
+	
+	public int getRctg_no() {
+		return rctg_no;
+	}
+	public void setRctg_no(int rctg_no) {
+		this.rctg_no = rctg_no;
+	}
+	public String getRctg_name() {
+		return rctg_name;
+	}
+	public void setRctg_name(String rctg_name) {
+		this.rctg_name = rctg_name;
+	}
+	
+	
 }
