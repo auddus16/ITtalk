34095491a289ef8b55e7cabafea9ba88aeba9005
jongwon <awosung00@naver.com>
2021-06-13 23:53:04 +0900

    front컨트롤러 수정

--- src/controller/FrontController.java
+++ src/controller/FrontController.java
@@ -1,124 +1,121 @@
-//package controller;
-//
-//import java.io.IOException;
-//
-//import javax.servlet.RequestDispatcher;
-//import javax.servlet.ServletException;
-//import javax.servlet.annotation.WebServlet;
-//import javax.servlet.http.HttpServlet;
-//import javax.servlet.http.HttpServletRequest;
-//import javax.servlet.http.HttpServletResponse;
-//
-//import test.ActionForward;
-//import test.MainAction;
-//
-///**
-// * Servlet implementation class FrontController
-// */
-//@WebServlet("*.jsp")
-//public class FrontController extends HttpServlet {
-//	private static final long serialVersionUID = 1L;
-//
-//	/**
-//	 * @see HttpServlet#HttpServlet()
-//	 */
-//	public FrontController() {
-//		super();
-//		// TODO Auto-generated constructor stub
-//	}
-//
-//	/**
-//	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-//	 */
-//	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-//		actionDo(request,response);
-//	}
-//
-//	/**
-//	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
-//	 */
-//	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-//		actionDo(request,response);
-//	}
-//
-//	private void actionDo(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
-//		String uri=req.getRequestURI();
-//		//System.out.println("uri: "+uri);
-//
-//		String cp=req.getContextPath();
-//		//System.out.println("cp: "+cp);
-//		// 프로그램이 실행되기 위해 필요한 리소스의 명칭
-//		// 메모리에 로드되기위한 메모리영역
-//
-//		String command=uri.substring(cp.length());
-//		System.out.println("command: "+command);
-//
-//		// XxxAction.java 파일들을 작성할 예정
-//		// Controller : M->보낼 데이터의 유무   V->어디로 보낼지
-//		ActionForward forward=null;
-//		if(command.equals("/hmy_main.do")) {
-//			try {
-//				forward=new MainAction().execute(req, res);
-//			}catch(Exception e) {
-//				e.printStackTrace();
-//			}
-//		}
-//		else if(command.equals("/회원가입.do")) {
-//			forward=new ActionForward();
-//			forward.setRedirect(false);
-//			forward.setPath("form.jsp");
-//		}
-//		else if(command.equals("/로그인.do")) {
-//
-//		}
-//		else if(command.equals("/id,pw찾기.do")) {
-//			
-//		}
-//		else if(command.equals("/마이페이지.do")) {
-//
-//		}
-//		else if(command.equals("/관리자.do")) {
-//
-//		}
-//		else if(command.equals("/게시판.do")) {
-//
-//		}
-//		else {
-//			forward=new ActionForward();
-//			forward.setRedirect(false);
-//			forward.setPath("error.jsp");
-//		}
-//
-//		if(forward!=null) {//방식과 경로가 있다면
-//			if(forward.isRedirect()) {// 방식이 있다면
-//				res.sendRedirect(forward.getPath());
-//			}
-//			else {//요청 재지정
-//				RequestDispatcher dispatcher = req.getRequestDispatcher(forward.getPath());
-//				dispatcher.forward(req, res);
-//				
-//				// 요청 지정 등의 기능에 사용되는 API
-//				// 클라이언트로부터 요청 받은 Servlet 프로그램이 응답하지않고
-//				// 다른 자원에 수행의 제어권을 넘겨서 처리 결과를 대신 응답하는 기능
-//				// or 다른 자원에 수행을 시키고 그 결과를 포함해서 응답하는 기능
-//				// == 요청 재지정
-//
-//				// 코드작성에 제한이 발생하기때문에(별도의 알고리즘 구현이 필요) << 세션,어플리케이션
-//				// RD를 get()할때, 경로를 인자로 넣어주면
-//				// 경로까지 데이터 전달가능
-//			}
-//		}
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//	}
-//
-//}
+package controller;
+
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class FrontController
+ */
+@WebServlet("*.do")
+public class FrontController extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * @see HttpServlet#HttpServlet()
+	 */
+	public FrontController() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		actionDo(request,response);
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		actionDo(request,response);
+	}
+
+	private void actionDo(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
+		String uri=req.getRequestURI();
+		//System.out.println("uri: "+uri);
+
+		String cp=req.getContextPath();
+		//System.out.println("cp: "+cp);
+		// 프로그램이 실행되기 위해 필요한 리소스의 명칭
+		// 메모리에 로드되기위한 메모리영역
+
+		String command=uri.substring(cp.length());
+		System.out.println("command: "+command);
+
+		// XxxAction.java 파일들을 작성할 예정
+		// Controller : M->보낼 데이터의 유무   V->어디로 보낼지
+		ActionForward forward=null;
+		if(command.equals("/hmy_main.do")) {
+			try {
+				forward=new MainAction().execute(req, res);
+			}catch(Exception e) {
+				e.printStackTrace();
+			}
+		}
+		else if(command.equals("/회원가입.do")) {
+			forward=new ActionForward();
+			forward.setRedirect(false);
+			forward.setPath("form.jsp");
+		}
+		else if(command.equals("/로그인.do")) {
+
+		}
+		else if(command.equals("/id,pw찾기.do")) {
+			
+		}
+		else if(command.equals("/마이페이지.do")) {
+
+		}
+		else if(command.equals("/관리자.do")) {
+
+		}
+		else if(command.equals("/게시판.do")) {
+
+		}
+		else {
+			forward=new ActionForward();
+			forward.setRedirect(false);
+			forward.setPath("error.jsp");
+		}
+
+		if(forward!=null) {//방식과 경로가 있다면
+			if(forward.isRedirect()) {// 방식이 있다면
+				res.sendRedirect(forward.getPath());
+			}
+			else {//요청 재지정
+				RequestDispatcher dispatcher = req.getRequestDispatcher(forward.getPath());
+				dispatcher.forward(req, res);
+				
+				// 요청 지정 등의 기능에 사용되는 API
+				// 클라이언트로부터 요청 받은 Servlet 프로그램이 응답하지않고
+				// 다른 자원에 수행의 제어권을 넘겨서 처리 결과를 대신 응답하는 기능
+				// or 다른 자원에 수행을 시키고 그 결과를 포함해서 응답하는 기능
+				// == 요청 재지정
+
+				// 코드작성에 제한이 발생하기때문에(별도의 알고리즘 구현이 필요) << 세션,어플리케이션
+				// RD를 get()할때, 경로를 인자로 넣어주면
+				// 경로까지 데이터 전달가능
+			}
+		}
+
+
+
+
+
+
+
+
+
+
+
+	}
+
+}
