67f1ea60bf768bd6a8ff83b4907e880a64cae44e
JungJun <ssikhj1214@gmail.com>
2021-06-13 10:37:01 +0900

    인코딩 2차수정

--- src/set/EncodingFilter.java
+++ src/set/EncodingFilter.java
@@ -35,7 +35,6 @@ public class EncodingFilter implements Filter {
 	 * @see Filter#doFilter(ServletRequest, ServletResponse, FilterChain)
 	 */
 	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
-		System.out.println("»Æ¿Œ");
 		request.setCharacterEncoding(encoding);
 		
 		
