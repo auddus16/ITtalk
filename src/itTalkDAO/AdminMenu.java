0d64f90d587126d186698c5ed9439e49c14d5ec0
kms152000 <kms152000@naver.com>
2021-06-17 12:37:42 +0900

    수정

--- src/itTalkDAO/AdminMenu.java
+++ src/itTalkDAO/AdminMenu.java
@@ -2,6 +2,11 @@ package itTalkDAO;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+
+import itTalkDO.Rb;
 
 public class AdminMenu {
 
@@ -9,6 +14,40 @@ public class AdminMenu {
 	PreparedStatement pstmt;
 	
 	// 신고 받은 게시글 목록 전체 출력
+	
+	public ArrayList<Rb> getReportBoard() {
+		ArrayList<Rb> reportBoards=new ArrayList();
+		
+		conn= DBManager.connect();
+		String sql="select * FROM Rb";
+		try {
+			pstmt=conn.prepareStatement(sql);
+			ResultSet rs=pstmt.executeQuery();
+			while(rs.next()) {
+				
+				Rb rb = new Rb();
+				rb.setRb_no(rs.getInt("rb_no"));
+				rb.setB_no(rs.getInt("b_no"));
+				rb.setMb_no(rs.getInt("mb_no"));
+				rb.setRctg_no(rs.getInt("rctg_no"));
+				rb.setRb_write(rs.getString("rb_write"));
+				rb.setRb_date(rs.getString("rb_date"));
+			}
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} finally {
+			try {
+				pstmt.close();
+				conn.close();
+			} catch (SQLException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+		return reportBoards;
+		
+	}
 	
 	// 조회 클릭시 해당 신고 받은 게시글 출력
 	
--- src/itTalkDAO/MainDAO.java
+++ src/itTalkDAO/MainDAO.java
@@ -2,6 +2,10 @@ package itTalkDAO;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+
 
 public class MainDAO {
 	
@@ -9,8 +13,54 @@ public class MainDAO {
 	PreparedStatement pstmt;
 	
 	// 실시간 인기 게시글 10개(조회수+댓글수:1시간마다 업뎃)
+	public ArrayList<String> getHits() {
+		conn= DBManager.connect();
+		ArrayList<String> Hits=new ArrayList();
+		String sql="SELECT b_no,mb_no,bc_no,b_title,b_write,b_file,b_date,b_hits,b_deleted,b_report,b_cnt,b_hits+b_cnt as b_total from B limit 0,10 Order by desc";
+		
+		try {
+			pstmt=conn.prepareStatement(sql);
+			ResultSet rs=pstmt.executeQuery();
+			while(rs.next()) {
+				Hits.add(rs.getString("b_no"));
+				Hits.add(rs.getString("mb_no"));
+				Hits.add(rs.getString("bc_no"));
+				Hits.add(rs.getString("b_title"));
+				Hits.add(rs.getString("b_write"));
+				Hits.add(rs.getString("b_file"));
+				Hits.add(rs.getString("b_date"));
+				Hits.add(rs.getString("b_hits"));		
+				Hits.add(rs.getString("b_deleted"));
+				Hits.add(rs.getString("b_report"));
+				Hits.add(rs.getString("b_cnt"));
+				Hits.add(rs.getString("b_total"));
+			}
+			
+		}catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} finally {
+			try {
+				pstmt.close();
+				conn.close();
+			} catch (SQLException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+		
+		return Hits;
+	}
+	
+	
+	// 메인에 출력 메소드
 	
 	
 	
 	// 제목&내용 검색 기능 -> 검색한 게시물 목록 전체 출력
+	
+	//select employee_id, first_name
+	//from EMPLOYEES
+	//where first_name like '%e%';
+	
 }
