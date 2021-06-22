16b84f914b998d8da107c77b42e717d2fe858f6c
JungJun <ssikhj1214@gmail.com>
2021-06-23 08:29:52 +0900

    로그인 추가

--- src/itTalkDAO/Login.java
+++ src/itTalkDAO/Login.java
@@ -68,5 +68,68 @@ public class Login {
 			}
 			return false;
 		}
+		
+		//아이디 찾기
+		public String idSearch(String mb_email) {
+			String id = null;
+			conn= DBManager.connect();
+			
+			String sql="select mb_id from mb where mb_email=?";
+			try {
+				pstmt=conn.prepareStatement(sql);
+				pstmt.setString(1, mb_email);
+				ResultSet rs=pstmt.executeQuery();
+				if(rs.next()) {
+					id = rs.getString("mb_id");
+				}
+			} catch (SQLException e) {
+				// TODO Auto-generated catch block
+				System.out.println("관련된 아이디가 없습니다");
+				e.printStackTrace();
+				return null;
+			} finally {
+				try {
+					pstmt.close();
+					conn.close();
+				} catch (SQLException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+			}
+			System.out.println("아이디 찾기 성공");
+			return id;
+		}
+		
+		//비밀번호 찾기
+		public String pwSearch(String mb_id) {
+			String pw = null;
+			conn= DBManager.connect();
+			
+			String sql="select mb_pw from mb where mb_id=?";
+			try {
+				pstmt=conn.prepareStatement(sql);
+				pstmt.setString(1, mb_id);
+				ResultSet rs=pstmt.executeQuery();
+				if(rs.next()) {
+					pw = rs.getString("mb_pw");
+				}
+			} catch (SQLException e) {
+				// TODO Auto-generated catch block
+				System.out.println("관련된 비밀번호가 없습니다");
+				e.printStackTrace();
+				return null;
+			} finally {
+				try {
+					pstmt.close();
+					conn.close();
+				} catch (SQLException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+			}
+			System.out.println("비밀번호 찾기 성공");
+			return pw;
+		}
+		
 	
 }
