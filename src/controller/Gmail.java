8fbe1f527b8aaec5715b583b65905a00aa24c5cf
JungJun <ssikhj1214@gmail.com>
2021-06-28 22:20:50 +0900

    수정

--- src/controller/Gmail.java
+++ src/controller/Gmail.java
@@ -6,7 +6,7 @@ import javax.mail.PasswordAuthentication;
 public class Gmail extends Authenticator { //아이디 패스워드 인증받기 함수
 	  PasswordAuthentication pa;
 	  public Gmail(){
-	    pa=new PasswordAuthentication("ssikhj1214@gmail.com","kkhji1024@");        
+	    pa=new PasswordAuthentication("ssikhj1214@gmail.com","@@@");        
 	  }
 	  @Override
 	  protected PasswordAuthentication getPasswordAuthentication() {
